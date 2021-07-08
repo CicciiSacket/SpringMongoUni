6a3b07db59ab84a35986fe35914ef5bb0ff50e80
Francesco Sacco <account_lavoro@Francescos-MBP.homenet.telecomitalia.it>
2021-07-08 13:13:56 +0200

    Started teachers controller test

---
+++ src/test/java/com/aitho/Controller/TeacherControllerTest.java
@@ -0,0 +1,22 @@
+package com.aitho.Controller;
+
+import com.aitho.Models.Teacher;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class TeacherControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+}
